daed1492fbc0b20e0fabf4c7a76c224f8f7b2496
sc0ttbrown <79423915+sc0ttbrown@users.noreply.github.com>
2023-06-15 14:52:51 +0100

    Added title to index.html

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ function Menu() {
 
 function Pizza({ pizzaObject }) {
   return (
-    <li className={`pizza ${pizzaObject.soldOut ? 'sold-out' : ''}`}>
+    <li className={`pizza${pizzaObject.soldOut ? ' sold-out' : ''}`}>
       <img src={pizzaObject.photoName} alt={pizzaObject.name} />
       <div>
         <h3>{pizzaObject.name}</h3>
